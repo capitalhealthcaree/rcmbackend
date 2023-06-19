a70ea52faf09a1471b6860d88678af070d3c6c70
capitalhealthcaree <premierpatientappointment@gmail.com>
2023-06-19 11:10:43 +0500

    remove patient keyword

--- controllers/demoRequest.js
+++ controllers/demoRequest.js
@@ -2,36 +2,36 @@ const nodemailer = require("nodemailer");
 const DemoRequest = require("../model/demoRequest");
 
 const welocome = (req, res) => {
-	res.send("Welcome RCM APIs");
+  res.send("Welcome RCM APIs");
 };
 
 const createDemoRequest = async (req, res) => {
-	const { name, phone, email, organization, numberOfPhysicians, message } =
-		req.body;
+  const { name, phone, email, organization, numberOfPhysicians, message } =
+    req.body;
 
-	try {
-		const demoData = await DemoRequest.create({
-			name,
-			phone,
-			email,
-			organization,
-			numberOfPhysicians,
-			message,
-		});
-		// Send an email to the admin
-		const transporter = nodemailer.createTransport({
-			service: "gmail",
-			auth: {
-				user: "webdevelopercapital@gmail.com",
-				pass: "ulgfizctvtqktylr",
-			},
-		});
+  try {
+    const demoData = await DemoRequest.create({
+      name,
+      phone,
+      email,
+      organization,
+      numberOfPhysicians,
+      message,
+    });
+    // Send an email to the admin
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "webdevelopercapital@gmail.com",
+        pass: "ulgfizctvtqktylr",
+      },
+    });
 
-		const mailOptions = {
-			from: email,
-			to: "webdevelopercapital@gmail.com",
-			subject: "Demo Request Details",
-			html: `
+    const mailOptions = {
+      from: email,
+      to: "webdevelopercapital@gmail.com",
+      subject: "Demo Request Details",
+      html: `
 			<html>
 			  <head>
 				<style>
@@ -45,7 +45,7 @@ const createDemoRequest = async (req, res) => {
 				</style>
 			  </head>
 			  <body>
-				<h1>Patient Details</h1>
+				<h1>Details</h1>
 				<p>Name : ${name}</p>
 				<p>Email : ${email}</p>
 				<p>organization : ${organization}</p>
@@ -54,45 +54,45 @@ const createDemoRequest = async (req, res) => {
 				<p>Message : ${message}</p>
 			  </body>
 			</html>`,
-		};
+    };
 
-		transporter.sendMail(mailOptions, (err, info) => {
-			if (err) {
-				console.error(err);
-			} else {
-				console.log(info);
-			}
-		});
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(info);
+      }
+    });
 
-		res
-			.status(200)
-			.json({ data: demoData, mesasge: "Demo reuqest sent successfully" });
-	} catch (error) {
-		res.status(500).json({ message: error.message });
-	}
+    res
+      .status(200)
+      .json({ data: demoData, mesasge: "Demo reuqest sent successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const getDemoRequestsByPagination = async (req, res) => {
-	const page = parseInt(req.query.page) || 1; // default to first page if page is not specified
-	const limit = parseInt(req.query.limit) || 21; // default to 10 documents per page if limit is not specified
-	const startIndex = (page - 1) * limit;
+  const page = parseInt(req.query.page) || 1; // default to first page if page is not specified
+  const limit = parseInt(req.query.limit) || 21; // default to 10 documents per page if limit is not specified
+  const startIndex = (page - 1) * limit;
 
-	try {
-		const totalDocs = await DemoRequest.countDocuments();
-		const data = await DemoRequest.find().skip(startIndex).limit(limit);
+  try {
+    const totalDocs = await DemoRequest.countDocuments();
+    const data = await DemoRequest.find().skip(startIndex).limit(limit);
 
-		res.status(200).json({
-			currentPage: page,
-			totalPages: Math.ceil(totalDocs / limit),
-			data,
-		});
-	} catch (err) {
-		res.status(500).json({ err: "getting some error" });
-	}
+    res.status(200).json({
+      currentPage: page,
+      totalPages: Math.ceil(totalDocs / limit),
+      data,
+    });
+  } catch (err) {
+    res.status(500).json({ err: "getting some error" });
+  }
 };
 
 module.exports = {
-	welocome,
-	createDemoRequest,
-	getDemoRequestsByPagination,
+  welocome,
+  createDemoRequest,
+  getDemoRequestsByPagination,
 };
